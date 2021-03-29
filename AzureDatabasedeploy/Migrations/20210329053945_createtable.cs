0a605875a14950cab6636d841ad82ce3a8d73d84
thejasv <48196639+thejasv@users.noreply.github.com>
2021-03-29 11:14:32 +0530

    create

---
+++ AzureDatabasedeploy/Migrations/20210329053945_createtable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AzureDatabasedeploy.Migrations
+{
+    public partial class createtable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Duration = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "Id", "Description", "Duration", "Name" },
+                values: new object[] { 1, "good", 2.05f, "xman" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movies");
+        }
+    }
+}
---
+++ AzureDatabasedeploy/Models/MovieContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureDatabasedeploy.Models
+{
+    public class MovieContext:DbContext
+    {
+        public MovieContext(DbContextOptions options):base(options)
+        {
+
+        }
+        public DbSet<Movie> Movies { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>().HasData(
+                new Movie
+                {
+                    Id = 1,
+                    Name = "xman",
+                    Description = "good",
+                    Duration = 2.05f
+                });
+        }
+    }
+}
